dfe24597bf4126ef82d275a55015d52ece05d8d4
Lucas Barboza <lucas_ti@uniaomundial.com.br>
2021-08-16 12:00:38 -0300

    Realizado o exercício 5.a e 5.b

--- 5.php
+++ 5.php
@@ -6,7 +6,33 @@
         [ "nome" => 'Galileo', "sobrenome" => 'Galilei', "nasc" => 1564, "morte" => 1642 ],
         [ "nome" => 'Nicolaus', "sobrenome" => 'Copernicus', "nasc" => 1473, "morte" => 1543 ],
         [ "nome" => 'Ada', "sobrenome" => 'Lovelace', "nasc" => 1815, "morte" => 1852 ]
-        ];
+    ];
 
+
+
+    // Questão 5.a
+    $aSobrnomeViveu = [retornaSobrenomeIdade($inventores)];
+    function retornaSobrenomeIdade ($array){
+        $i = 0;
+        foreach($array as $inventor){           
+            $array[$i] = [ 'sobrenome' => $inventor['sobrenome'], 'viveu' => $inventor['morte']-$inventor['nasc']];
+            $i++;
+        }
+        return $array;
+    }
+
+
+    // Questão 5.b
+    $media = retornaMediaAnos($inventores);
+    function retornaMediaAnos ($array){
+        $idade = [];
+        foreach($array as $inventor){           
+            array_push( $idade, $inventor['morte']-$inventor['nasc'] );
+        }
+
+        return array_sum($idade) / count( $idade );
+    }
+
+    // Questão 5.c
+    
 ?>
-
